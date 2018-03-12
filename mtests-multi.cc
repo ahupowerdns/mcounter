5e72d798b7d412f4697a6ffa82db98559bb02602
bert hubert <bert.hubert@netherlabs.nl>
2018-03-12 09:10:57 +0100

    add torture test for 'multiple' case

--- mtests-multi.cc
+++ mtests-multi.cc
@@ -3,11 +3,15 @@
 #include <thread>
 #include <unistd.h>
 
-TEST_CASE("Mcounter multiple basic", "[multbas]") {
+namespace {
   struct mystruct
   {
     uint64_t a,b;
   };
+}
+
+
+TEST_CASE("Mcounter multiple basic", "[multbas]") {
   UnsharedCounterStructParent<mystruct> ucp;
   
   auto func = [](UnsharedCounterStructParent<mystruct>* ucp) {
@@ -24,11 +28,8 @@ TEST_CASE("Mcounter multiple basic", "[multbas]") {
   REQUIRE(ucp.get().b == 0);
 }
 
+
 TEST_CASE("Mcounter multiple move", "[multmove]") {
-  struct mystruct
-  {
-    uint64_t a,b;
-  };
   UnsharedCounterStructParent<mystruct> ucp;
 
   auto func = [](UnsharedCounterStructParent<mystruct>* ucp) {
@@ -47,3 +48,23 @@ TEST_CASE("Mcounter multiple move", "[multmove]") {
   REQUIRE(ucp.get().a == 20000000);
   REQUIRE(ucp.get().b == 40000000);
 }
+
+TEST_CASE("Mcounter multi torture", "[mtorture]") {
+  UnsharedCounterStructParent<mystruct> ucp;
+
+  auto func = [](UnsharedCounterStructParent<mystruct>* ucp) {
+    for(int m=0 ; m < 1000; ++m) {
+      auto ucs = ucp->getLocal();
+      for(unsigned int n = 0; n < 10000; ++n)
+        ++ucs.d_value.a;
+    }
+  };
+
+  for(int n=0; n < 10; ++n) {
+    std::thread t1(func, &ucp), t2(func, &ucp);
+    t1.join();
+    t2.join();
+  }
+  REQUIRE(ucp.get().a == 200000000);
+}
+
